02f0106aa57385f892ce1acfb758fd8586d04942
stephan <railsc0d0r@gmail.com>
2015-11-28 05:44:01 +0100

    Adds factories for users and roles to use in tests. Refs #54

---
+++ test/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :username do |n|
+      "myLogin#{n}"
+    end
+    sequence :email do |n|
+      "my_mail#{n}@global8ball.com"
+    end
+    password "secret123456789"
+    password_confirmation "secret123456789"
+    sign_in_count 0
+    activated true
+  end
+
+end
