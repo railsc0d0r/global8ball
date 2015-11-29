8b88f50e59f605b5441f30b823e846003b2204d8
stephan <railsc0d0r@gmail.com>
2015-11-29 05:07:49 +0100

    Adds authentication-token to user. Refs #54

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128203613) do
+ActiveRecord::Schema.define(version: 20151129040431) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151128203613) do
     t.string   "username"
     t.boolean  "activated",              default: false
     t.integer  "role_id"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
