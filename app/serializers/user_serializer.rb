b1731bd5e59e9a505e30d9940243ba562de2d2bf
stephan <railsc0d0r@gmail.com>
2015-11-30 22:19:30 +0100

    Provides user-obj as JSON. Refs #54

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :username, :email
+
+  belongs_to :role
+  
+  def role_name
+    object.role.try :name
+  end
 end
