826ba3bd0113616120e1e4884d0483c17b068541
stephan <railsc0d0r@gmail.com>
2015-11-30 01:47:34 +0100

    Adds a way to transport asset-url from backend to app.

---
+++ app/helpers/ember_helper.rb
@@ -0,0 +1,18 @@
+module EmberHelper
+  # From https://github.com/thoughtbot/ember-cli-rails/issues/30#issuecomment-78183246
+  def image_assets
+    image_asset_dir = Rails.root.join('app', 'assets', 'images')
+
+    assets = {}
+
+    %w(.jpg .png .jpeg .gif .svg).each do |file_ext|
+      Dir.glob(File.join(image_asset_dir, "**", "*#{file_ext}")).each do |absolute_path|
+        file = absolute_path.sub(File.join(image_asset_dir, '/'), '')
+        assets[file] = asset_url(file)
+      end
+    end
+
+    assets
+  end
+
+end
