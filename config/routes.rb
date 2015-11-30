2d3153a4457581ac41018a8df9f376a4356c0e79
stephan <railsc0d0r@gmail.com>
2015-11-30 01:46:48 +0100

    Prepares backend to provide user-model over REST. Refs #54

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # scope the backend to provide deeplinks for ember-app
+  scope '/backend' do
+    devise_for :users, controllers: { sessions: 'sessions' }
+    resources :users
+  end
   mount_ember_app :frontend, to: "/", controller: "application", action: "index"
 end
