39dd1ea4cd3de8ad8bd28be6747fe1af0428b453
stephan <railsc0d0r@gmail.com>
2015-12-07 00:37:22 +0100

    Fills employee-model. Refs #54, 59, #60, #61

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,22 @@
 class Employee < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :person
+  include UserConcern
+  include PersonConcern
+
+  attr_accessor :skip_setting_username_and_email
+  
+  before_validation :set_username, on: :create, unless: :skip_setting_username_and_email
+  before_validation :set_user_email_from_person, unless: :skip_setting_username_and_email
+
+  protected
+
+  def set_username
+    unless  %w( stephan ).include? self.user.username
+      user.username = "#{person.lastname.downcase}_#{person.firstname.downcase}_#{StringRandom.new.password}"
+    end
+  end
+
+  def set_user_email_from_person
+    user.email = person.email if person.email
+  end
+
 end
