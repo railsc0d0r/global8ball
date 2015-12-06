f5ebb14952c6ea745db5601327e662654aaf85ca
stephan <railsc0d0r@gmail.com>
2015-12-07 00:36:37 +0100

    Fills player-model. Refs #54, #55, #56, #57, #58

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,18 @@
 class Player < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :person
+  include UserConcern
+  include PersonConcern
+
+  ensure_user_role 'Player'
+
+  after_rollback :clean_up_associations, on: :create
+
+  protected
+
+  # called when saving has failed for some reason, but some of the associated
+  # records may already have been persisted.
+  def clean_up_associations
+    user.destroy if user.try :persisted?
+    person.destroy if person.try :persisted?
+  end
+  
 end
