aa45733d5ab32142fcc3e33311d2ee8e249fe273
stephan <railsc0d0r@gmail.com>
2015-12-10 21:09:54 +0100

    Fills factory for person. Refs #54, #55, #56, #59, #60

--- test/factories/people.rb
+++ test/factories/people.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :person do
-    
+    sequence :lastname do |n|
+      "Lastname_#{n}"
+    end
+    sequence :firstname do |n|
+      "Firstname_#{n}"
+    end
+    sequence(:email) { |n| "person-#{n}@example.com" }
+    association :address, factory: :address, strategy: :build
   end
-
 end
