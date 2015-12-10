79a44547bdc9f1ec9fbf10db603a139726724ac7
stephan <railsc0d0r@gmail.com>
2015-12-10 04:57:56 +0100

    Scaffolds player-controller and adds it as resource to routing in backend. Refs #54, #55, #56, #57

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { sessions: 'sessions' }
     resources :users
     resources :employees
+    resources :players
   end
   mount_ember_app :frontend, to: "/", controller: "application", action: "index"
 end
