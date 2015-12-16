e793134bd29c8adcb8aa978ee15b3bd0a7921102
stephan <railsc0d0r@gmail.com>
2015-12-16 01:22:34 +0100

    Makes interface to countries more consistent.

--- app/models/country.rb
+++ app/models/country.rb
@@ -8,19 +8,32 @@ class Country
   end
 
   def self.find_by_id id
-    ISO3166::Country.find_country_by_number(id.to_s).data
+    data = ISO3166::Country.find_country_by_number(id.to_s).data
+    parse_data data
   end
 
   def self.find_by_name name
-    ISO3166::Country.find_country_by_name(name).data
+    data = ISO3166::Country.find_country_by_name(name).data
+    parse_data data
   end
 
   def self.find_by_code code
-    ISO3166::Country.find_country_by_un_locode(code).data
+    data = ISO3166::Country.find_country_by_un_locode(code).data
+    parse_data data
   end
 
   def self.find_all_by_region name
-    ISO3166::Country.find_all_countries_by_region(name).map{|c| c.data}
+    ISO3166::Country.find_all_countries_by_region(name).map{|c| {id: c.number, name: c.name, code: c.un_locode}}
+  end
+
+  private
+
+  def parse_data data
+    {
+      id: data['number'],
+      name: data['name'],
+      code: data['un_locode']
+    }
   end
 
 end
