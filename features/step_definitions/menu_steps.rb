d551d820c1e97b094bca2e9884db63e16f3df461
stephan <railsc0d0r@gmail.com>
2015-12-31 17:28:36 +0100

    Adds steps belonging to language-selection. Refs #72

--- features/step_definitions/menu_steps.rb
+++ features/step_definitions/menu_steps.rb
@@ -6,3 +6,18 @@ Wenn(/^ich aus dem Admin\-Menü "(.*?)" auswähle\.$/) do |link_name|
          }
 end
 
+Wenn(/^ich die Sprachauswahl öffne\.$/) do
+  steps %{ Wenn ich den "toggle_language_selection"-Link klicke. }
+end
+
+Wenn(/^ich "(.*?)" als Sprache aus dem Dropdown auswähle\.$/) do |language|
+  language_dropdown = page.find_by_id "language_selection"
+
+  within language_dropdown do
+    if Capybara.current_driver == :poltergeist
+      find(:link, language).trigger('click')
+    else
+      click_link(language)
+    end
+  end
+end
