af988f72a23491cc185087559762a57d20d3b3f8
stephan <railsc0d0r@gmail.com>
2016-01-12 03:25:47 +0100

    Sets whodunnit for papertrail from current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user_from_token!, unless: :devise_controller?
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_paper_trail_whodunnit
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
