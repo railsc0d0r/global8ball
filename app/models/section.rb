8c4a19cc6c9e5395b9743a28bcaf06d31cabfba7
stephan <railsc0d0r@gmail.com>
2016-01-16 06:23:38 +0100

    Configures background in section-model. Refs #62, #77

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,10 @@ class Section < ActiveRecord::Base
 
   has_paper_trail
 
+  has_attached_file :background, styles: { large: "1400x1050>", thumb: "100x100>" }
+
+  validates_attachment_content_type :background, content_type: /\Aimage\/.*\Z/
+
   has_many :contents, inverse_of: :section, dependent: :destroy
 
 end
