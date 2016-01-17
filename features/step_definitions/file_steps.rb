6c3a7ecd79c777c3ad3c7c68d55a9d558e4873cc
stephan <railsc0d0r@gmail.com>
2016-01-17 04:58:35 +0100

    Adds stepdefinition for choosing an image to upload. Refs #62, #77

--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -2,3 +2,10 @@
 Angenommen(/^eine Datei "(.*?)" im Testfixtures\-Verzeichnis\.$/) do |filename|
   File.file?(TestFiles.folder_path.join(filename))
 end
+
+Wenn(/^ich das Bild "(.*?)" als Hintergrundbild auswähle\.$/) do |arg1|
+  # We inject the encoded data for the image directly into the app-controller because of a bug in phantomjs.
+  # Beware: this is not implementation-agnostic!!!
+  data_url = DataUrl.from_image filename
+  evaluate_script("Frontend.__container__.lookup('controller:section.upload-background').set('background',#{data_url.to_json});")
+end
