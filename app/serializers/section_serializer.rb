5fb0048a5f4fbdd1fd5ccdf47714ea85283d1ed9
stephan <railsc0d0r@gmail.com>
2016-01-18 23:43:18 +0100

    Adds background as property to be serialized. Refs #62, #77

--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,9 +1,13 @@
 class SectionSerializer < ActiveModel::Serializer
-  attributes :id, :path
+  attributes :id, :path, :background
 
   has_many :contents
 
   def contents
     object.contents.pluck :id
+  end
+
+  def background
+    object.background.url(:large) unless object.background.url(:large) == "/backgrounds/large/missing.png"
   end
 end
