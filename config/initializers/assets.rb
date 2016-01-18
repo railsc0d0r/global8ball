43d0cfeaaae06b04560feb1c933ab03bc9e8f920
stephan <railsc0d0r@gmail.com>
2016-01-18 23:42:37 +0100

    Configures paperclip to use another url and path. Refs #62, #77

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Creates RailsUpload-folder, if it doesn't exist
+RailsUpload.setup
+
+# Set paperclip to use this folder
+Paperclip::Attachment.default_options[:path] = "#{RailsUpload.folder_path}/:class/:attachment/:id_:style_:filename"
+Paperclip::Attachment.default_options[:url] = "#{RailsUpload.base_url}/:class/:attachment/:id_:style_:filename"
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
