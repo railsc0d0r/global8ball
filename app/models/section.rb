c0c38ce2ca8574ac6dac3c83190aa6398940cb02
stephan <railsc0d0r@gmail.com>
2016-01-20 04:44:18 +0100

    Lessens the dimensions of the converted images for section.background. Less is more. Refs #62, #77

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,10 @@ class Section < ActiveRecord::Base
 
   has_paper_trail
 
-  has_attached_file :background, styles: { large: "1400x1050>", thumb: "100x100>" }
+  has_attached_file :background, styles: {
+                      large: "1024x768>",
+                      thumb: "100x100>"
+                    }
 
   validates_attachment_content_type :background, content_type: /\Aimage\/.*\Z/
 
