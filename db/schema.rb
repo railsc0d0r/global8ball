2454b9bda4647ac35d1745683db3b8669316919d
stephan <railsc0d0r@gmail.com>
2016-02-08 21:27:28 +0100

    Adds migration and new db-schema for user-registration. Refs #54, #55, #56

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116051343) do
+ActiveRecord::Schema.define(version: 20160208202038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,8 +107,13 @@ ActiveRecord::Schema.define(version: 20160116051343) do
     t.integer  "role_id"
     t.string   "authentication_token"
     t.string   "auth_obj_type"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
