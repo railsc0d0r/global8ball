b44a6565eeeaba3ad82ffaf69824db4f3c5b1374
stephan <railsc0d0r@gmail.com>
2016-02-09 06:28:42 +0100

    Serializes auth_obj_type and auth_obj_id for user to let it get fetched from app. Refs #54, #55, #56

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :role_name
+  attributes :id, :username, :email, :role_name, :auth_obj_type, :auth_obj_id
 
   def role_name
     object.role.try :name
+  end
+
+  def auth_obj_id
+    object.auth_obj.try :id
   end
 end
