214f00b54612d4004d083e643b42c4ec1767c3af
stephan <railsc0d0r@gmail.com>
2016-02-21 04:29:50 +0100

    Fixes stepdefinition for checking last email. Refs #54, #55

--- features/step_definitions/custom_email_steps.rb
+++ features/step_definitions/custom_email_steps.rb
@@ -1,4 +1,11 @@
 # coding: utf-8
 Dann(/^möchte ich eine Email mit dem Betreff "(.*?)" bekommen haben\.$/) do |subject|
-  steps %{ Then "#{@player.email}" should receive an email with subject "#{subject}" }
+  @player = @player.nil? ? Player.last : @player
+
+  expect(unread_emails_for(@player.email).size).to eql 1
+
+  # this call will store the email and you can access it with current_email
+  open_last_email_for(@player.email)
+  expect(current_email).to have_subject(subject)
+
 end
