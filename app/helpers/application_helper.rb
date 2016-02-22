2e935244f207f90561aa37ca0615efd49398fe5c
Stephan Barth <stephan.barth@niemau.de>
2016-02-22 19:14:59 +0100

    Adds helper-methods to make stored request-values available in mailer-template. Refs #54, #55

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def protocol
+    RequestStore.store[:protocol]
+  end
+
+  def host
+    RequestStore.store[:host]
+  end
+
+  def port
+    RequestStore.store[:port] == 443 || RequestStore.store[:port] == 80 || RequestStore.store[:port].empty? ? "" : ":#{RequestStore.store[:port]}"
+  end
 end
