4a57bb39c7676dbde0b5b141994efa8072a25314
stephan <railsc0d0r@gmail.com>
2016-02-22 04:11:17 +0100

    Refactors stepdefinitions for player to check for negatives, too. Refs #54, #56

--- features/step_definitions/player_steps.rb
+++ features/step_definitions/player_steps.rb
@@ -1,8 +1,18 @@
 # coding: utf-8
-Dann(/^sollte dieser Spieler bestätigt sein\.$/) do
-  expect(@player.confirmed).to be_truthy
+Dann(/^sollte dieser Spieler( nicht|) bestätigt sein\.$/) do |nicht|
+  @player.reload
+  if nicht.size == 0
+    expect(@player.confirmed?).to be_truthy
+  else
+    expect(@player.confirmed?).to be_falsy
+  end
 end
 
-Dann(/^sollte dieser Spieler aktiviert sein\.$/) do
-  expect(@player.activated).to be_truthy
+Dann(/^sollte dieser Spieler( nicht|) aktiviert sein\.$/) do |nicht|
+  @player.reload
+  if nicht.size == 0
+    expect(@player.activated?).to be_truthy
+  else
+    expect(@player.activated?).to be_falsy
+  end
 end
