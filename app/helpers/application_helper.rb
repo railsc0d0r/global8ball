d8bdef2219718c0e028585bb9eb2af706a867b6c
stephan <railsc0d0r@gmail.com>
2016-02-23 04:55:12 +0100

    Fixes bug in application_helper: port is a fixnum and can't be empty, just probably nil. Refs #54, #55

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
   end
 
   def port
-    RequestStore.store[:port] == 443 || RequestStore.store[:port] == 80 || RequestStore.store[:port].empty? ? "" : ":#{RequestStore.store[:port]}"
+    RequestStore.store[:port] == 443 || RequestStore.store[:port] == 80 || RequestStore.store[:port].nil? ? "" : ":#{RequestStore.store[:port]}"
   end
 end
