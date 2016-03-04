c19aa983b907e914692f7ab42c22e5b9c52ebcbe
stephan <railsc0d0r@gmail.com>
2016-03-04 04:58:22 +0100

    Removes deprecation-msg by changing before_filter to before_action in controller. Refs #91

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   include ErrorSerializer
 
-  before_filter :authenticate_user_from_token!, unless: :devise_controller?
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  before_filter :set_paper_trail_whodunnit
-  before_filter :store_request_details
+  before_action :authenticate_user_from_token!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_paper_trail_whodunnit
+  before_action :store_request_details
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
