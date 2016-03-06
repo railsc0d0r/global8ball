f1fc976d3c4ca5805b9aeb0da94636fa9a7076d8
stephan <railsc0d0r@gmail.com>
2016-03-06 23:28:17 +0100

    Adds ember-apps to precompile-paths. Tells sprockets to precompile assets in these paths.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,8 +10,21 @@ RailsUpload.setup
 Paperclip::Attachment.default_options[:path] = "#{RailsUpload.folder_path}/:class/:attachment/:id_:style_:filename"
 Paperclip::Attachment.default_options[:url] = "#{RailsUpload.base_url}/:class/:attachment/:id_:style_:filename"
 
+ember_build_folder = Rails.root.join("tmp", "ember-cli","apps").to_s
+
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << ember_build_folder
+
+# Precompile ember-apps
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(html|map|xml|txt|css|js||eot|ttf|woff|fnt|svg|appcache|json)\z/
+    full_path = Rails.application.assets.resolve(path)
+    app_assets_path = ember_build_folder
+    full_path.starts_with?(app_assets_path) ? true : false
+  else
+    false
+  end
+end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
