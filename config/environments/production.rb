f5f077fe19f746e1604ad81734e07895c086f92a
stephan <railsc0d0r@gmail.com>
2016-03-13 05:09:40 +0100

    Configures action_cable-endpoint for dev and production. Refs #66, #93

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://preview.global8ball.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://preview.global8ball.com', 'https://global8ball.com', 'https://global8ball.ignorelist.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
