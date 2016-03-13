69d925ca492ca907ac2ea73face43999e07479e8
stephan <railsc0d0r@gmail.com>
2016-03-13 22:09:57 +0100

    Defines association and validation for shot-model. Refs #66, #97

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,7 +1,32 @@
 class Shot < Ohm::Model
   include Ohm::Timestamps
   include Ohm::DataTypes
+  include Ohm::Callbacks
 
   reference :round, :Round
   collection :positions, :Position
+  collection :collisions, :Collision
+
+  attribute :owner_id, Type::Integer
+
+  def before_save
+    validate_owner
+  end
+
+  def owner
+    Player.find(owner_id)
+  end
+
+  def owner= player
+    raise "Given object is not of class Player" unless player.is_a?(Player)
+    self.owner_id=player.id
+    self.save
+  end
+
+  private
+
+  def validate_owner
+    raise "Owner is not valid. Has to be '#{self.round.player_one.firstname} #{self.round.player_one.lastname}' or '#{self.round.player_two.firstname} #{self.round.player_two.lastname}'" unless self.owner_id == (self.round.game.player_1_id || self.round.game.player_2_id)
+  end
+
 end
