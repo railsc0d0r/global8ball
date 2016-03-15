d347a577862350d671078336340a2e455111c304
stephan <railsc0d0r@gmail.com>
2016-03-15 03:19:03 +0100

    Adds id_type and id_number to factory for a player.

--- test/factories/players.rb
+++ test/factories/players.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :player do
     association :user, strategy: :build
     association :person, strategy: :build
+    card_number '123456789'
+    id_type 'Passport'
+    id_number '123456789'
   end
 end
