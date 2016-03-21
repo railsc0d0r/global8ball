e32a1ed4495dce04256e810dd51925638676d846
stephan <railsc0d0r@gmail.com>
2016-03-21 21:44:13 +0100

    Tells devise to use our custom mailer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
   config.mailer_sender = 'no-reply@global8ball.com'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'MyMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
