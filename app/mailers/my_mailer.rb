9f6d35bccec1518b4942add776bfda1ac2dda038
stephan <railsc0d0r@gmail.com>
2016-03-21 20:58:06 +0100

    Implements inherited mailer to set special options.

---
+++ app/mailers/my_mailer.rb
@@ -0,0 +1,24 @@
+class MyMailer < < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+
+  def confirmation_instructions(record, token, opts={})
+    opts[:from] = 'noreply@global8ball.com'
+    opts[:reply_to] = 'contact@global8ball.com'
+    super
+  end
+
+  def reset_password_instructions(record, token, opts={})
+    opts[:from] = 'noreply@global8ball.com'
+    opts[:reply_to] = 'contact@global8ball.com'
+    super
+  end
+
+  def unlock_instructions(record, token, opts={})
+    opts[:from] = 'noreply@global8ball.com'
+    opts[:reply_to] = 'contact@global8ball.com'
+    super
+  end
+
+end
