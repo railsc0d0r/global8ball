15541ec702063cf29983bd731cd439ef5222512f
stephan <railsc0d0r@gmail.com>
2016-04-19 22:04:48 +0200

    Defines 1st behaviour for game-model. Refs #66, #92

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "With two players joining a game" do
+    FactoryGirl.create(:role, name: 'Player')
+    player1 = FactoryGirl.create(:player)
+    player2 = FactoryGirl.create(:player)
+
+    it "Validates that the two players are not the same" do
+      game = Game.new
+      game.player_one = player1
+
+      expect(game.player_two = player1).to raise_error
+    end
+  end
 end
