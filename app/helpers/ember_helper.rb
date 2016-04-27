20f43fcc561b26f6d49273f172d7f807c0d0f2ee
stephan <railsc0d0r@gmail.com>
2016-04-27 03:34:44 +0200

    Substitutes asset_url for paths. We just use them in frontend - so we need just the relative part of the url.

--- app/helpers/ember_helper.rb
+++ app/helpers/ember_helper.rb
@@ -8,7 +8,7 @@ module EmberHelper
     %w(.jpg .png .jpeg .gif .svg).each do |file_ext|
       Dir.glob(File.join(image_asset_dir, "**", "*#{file_ext}")).each do |absolute_path|
         file = absolute_path.sub(File.join(image_asset_dir, '/'), '')
-        assets[file] = asset_url(file)
+        assets[file] = asset_path(file)
       end
     end
 
