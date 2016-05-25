f88cc52706765ebaa672a5215e8f025219fcd3e7
stephan <railsc0d0r@gmail.com>
2016-05-25 06:40:15 +0200

    Includes helper from gem in controllers. Refs #101

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  # Include helper from Global8ballGame
+  helper Global8ballGame::GameHelper
+
   def index
     render layout: false
   end
